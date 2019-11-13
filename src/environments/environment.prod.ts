9fc9cc1e501215cc6a2f7b96c1f3afcbd41ddc65
Est <est.kungzc@gmail.com>
2019-11-13 22:18:37 +0700

    add hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBn_DUF0GrpPvbwMw8ZRl0d0iFw8j1c7RU",
+    authDomain: "sansara-farm.firebaseapp.com",
+    databaseURL: "https://sansara-farm.firebaseio.com",
+    projectId: "sansara-farm",
+    storageBucket: "sansara-farm.appspot.com",
+    messagingSenderId: "839152960284",
+    appId: "1:839152960284:web:7852a773b7437272e8f0d9",
+    measurementId: "G-TKH388907G"
+  }
 };
